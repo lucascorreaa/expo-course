9779a7487a2d8a48f9b04915c852549d7b6b5edf
lucascorreaa <lucas.lca14@gmail.com>
2024-05-07 21:46:20 -0300

    fix: clean app

--- App.js
+++ App.js
@@ -1,29 +1,10 @@
-import { StyleSheet, View, Text, Platform } from "react-native";
+import { View, Text } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>INDMO</Text>
-      <Text style={styles.text}>Hello World!</Text>
+    <View>
+      <Text>INDMO</Text>
+      <Text>Expo Course!</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: "1",
-    display: "flex",
-    backgroundColor: Platform.OS === "android" ? "blue" : "yellow",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    color: "purple",
-    fontSize: 20,
-    fontStyle: "italic",
-  },
-  text: {
-    color: "green",
-    fontSize: 20,
-  },
-});
