48111181f0a8bda1b3d4481353e96b8710e920f2
FelipeFVieira <devfelipefranca@gmail.com>
2024-05-27 22:35:51 -0300

    devoloping request api

---
+++ src/helpers/axiosBaseUrl.js
@@ -0,0 +1,14 @@
+const axios = require('axios');
+
+const baseURL = 'https://api.openweathermap.org/data/2.5';
+
+const baseRequest = axios.create({
+  baseURL: baseURL,
+  timeout: 10000, 
+  headers: {
+    'Content-Type': 'application/json',
+  }
+});
+
+// export default baseRequest;
+module.exports = baseRequest;
