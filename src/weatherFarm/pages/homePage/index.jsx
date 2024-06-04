ccf1489f26a4762f0a539534533a46c0e8a86680
PauloBezz <joaolucas.aqalves111@outlook.com>
2024-06-03 21:55:07 -0300

    added hours and week of day in project

--- src/weatherFarm/pages/homePage/index.jsx
+++ src/weatherFarm/pages/homePage/index.jsx
@@ -2,22 +2,35 @@ import React from 'react';
 
 import { useState } from 'react';
 
-import { Container } from "../../../class-component/native-component/styles";
-import { Image, Text, TextInput, TouchableOpacity, ImageBackground, View, Button } from "react-native"
+import { Image, Text, TextInput, ImageBackground, View, Button } from "react-native"
+
 
 export default function HomePage(){
 
+    const minutes = new Date().getMinutes();
+
+    const day = new Date().getDay();
+
+    const dayWeek = [ 'Dom', 'Seg', 'Ter', 'Quar', 'Qui', 'Sex', 'Sab'];
+
+
    const [city, setCity] = useState('São Paulo');
    const [data, setData] = useState({
-    temp: '19ºC'
+    temp: ''
    });
+
+   const [hours, setHours] = useState(new Date().getHours());
+
+   
+
+
 
    const getData = async () => {
     try {
       const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=pt_br&appid=f5f865ff13ae9828564399051f651125&units=metric`);
       const json = await response.json();
       setData({
-        temperature: json.main.temp
+        temperature: ((json.main.temp % 1 >= 0.5) ? Math.ceil(json.main.temp) + 'º' : Math.floor(json.main.temp) + 'º')
     });
     } catch (error) {
       console.error(error);
@@ -38,50 +51,124 @@ export default function HomePage(){
             gap: 50,
         }}>
 
-           <View style={{
-            display: 'flex',
-            flexDirection: 'row',
-           }}>
-                <TextInput 
-                    placeholder="Search Location" 
-                    onChangeText={handleSetCity}
-                    value={city}
-                    style={{
-                        backgroundColor: '#9d9d9d',
-                        width: 200,
-                        height: 30,
-                        borderRadius: 5,     
-                    }}
-                ></TextInput>
-                <Button 
-                onPress={getData}
-                title='Aqui'
-                > 
-                    <Image 
-                    source={require('./assets/img/Vector.png')}
-                    style={{
-                        width: 20,
-                        height: 20,
-                        marginRight: 10,
-                    }}/> 
-                 </Button>
-           </View>
-           <View>
-                    <Text style={{
-                        fontSize: 30,
-                        color: 'white',
-                        fontWeight: 500
-                    }}>{city}</Text>
-                <Text style={{
-                    fontSize: 100,
-                    color: 'white',
-                    fontWeight: 800,
-                }}>{data.temperature}</Text>
-           </View>
+
+                <View style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                }}>
+                        <TextInput 
+                            placeholder="Search Location" 
+                            onChangeText={handleSetCity}
+                            value={city}
+                            style={{
+                                backgroundColor: '#9d9d9d',
+                                width: 200,
+                                height: 30,
+                                borderRadius: 5,     
+                            }}
+                        ></TextInput>
+                        <Button 
+                        onPress={getData}
+                        title='Aqui'
+                        > 
+                            <Image 
+                            source={require('./assets/img/Vector.png')}
+                            style={{
+                                width: 20,
+                                height: 20,
+                                marginRight: 10,
+                            }}/> 
+                        </Button>
+                </View>
+                            <Image 
+                                source={require('../homePage/assets/img/iconSunMin.png')}
+                                style={{
+                                    width: 300,
+                                    height: 300,
+                                    marginRight: 10,
+                                }}
+                            />
+                <View style={{
+                    width: '100%',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                }}>
+                            <Text style={{
+                                fontSize: 30,
+                                color: 'white',
+                                fontWeight: 500,
+                            }}>{city}</Text>
+
+                            <View style={{
+                                display: 'flex',
+                                alignItems: 'center',
+                                justifyContent: 'center',
+                            }}>
+                                <Text style={{
+                                    fontSize: 60,
+                                    color: 'white',
+                                }}>--------------</Text>
+
+
+                                <View style={{
+                                    display: 'flex',
+                                    flexDirection: 'row',
+                                    gap: 20,
+                                }}>
+
+                                    <Text style={{
+                                        fontSize: 90,
+                                        color: 'white',
+                                        fontWeight: 800,
+                                    }}>{data.temperature}</Text>
+
+
+                                    <View style={{
+                                        display: 'flex',
+                                        alignItems: 'center',
+                                        justifyContent: 'center',
+                                    }}>
+                                        <Text style={{
+                                            fontSize: 35,
+                                            color: 'white',
+                                            fontWeight: 800,
+                                        }}>{dayWeek[day]}</Text>
+                                        <Text style={{
+                                            fontSize: 25,
+                                            color: 'white',
+                                            fontWeight: 800,
+                                        }}>{hours}h{minutes}m</Text>
+                                    </View>
+
+
+                                </View>
 
 
 
-                {/* <Image source={require('../homePage/assets/img/iconSunMin.png')} /> */}
+
+
+
+
+
+
+
+
+
+                            <Text style={{
+                                    fontSize: 60,
+                                    color: 'white',
+                                }}>--------------</Text>
+
+                            </View>
+
+
+
+                </View>
+
+
+
+
         </ImageBackground>
     )
 }
