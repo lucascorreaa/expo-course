7a12e460adcb7f517452aa55dc2598c215c7c71d
Maria Eduarda <mariaeduarda.nicacio2007@gmail.com>
2024-06-08 16:56:59 -0300

    feat: app js

--- App.js
+++ App.js
@@ -1,5 +1,19 @@
-import Search from "./src/type-weather/pages/search";
+import { ScrollView, View } from "react-native";
+import Card from "./src/type-weather/pages/dash/components/card";
+import WeatherDetail from "./src/type-weather/pages/dash/components/weather-detail";
+import NextDays from "./src/type-weather/pages/dash/components/next-days";
 
 export default function App() {
-  return <Search />;
+  return (
+    <ScrollView style={{backgroundColor: "#13131A"}}
+    contentContainerStyle={{
+      display: "flex",
+       flexDirection: "column", 
+       alignItems: "center"}}
+    >
+      <Card />
+      <WeatherDetail />
+      <NextDays />
+    </ScrollView>
+  )
 }
